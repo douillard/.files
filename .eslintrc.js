3d473a9719a1713f0c2de8b7ca6d46d445431bb0
Chris Douillard <cdouillard@me.com>
2018-03-25 13:14:30 -0600

    updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,20 +27,20 @@ module.exports = {
 
     "rules": {
         "react/prop-types": ["off"],
-        "indent": ["error", 2, { SwitchCase: 1 }],
-        "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "single"],
         "semi": ["error", "always"],
         "no-console": ["warn", { allow: ["info", "error"] }],
-        "arrow-parens": ["error", "always"],
-        
-        //        "react/jsx-uses-react": "error",
-        //        "react/jsx-uses-vars": "error",
-        //        "react/jsx-filename-extension": "error",
-        //        "react/self-closing-comp": "error",
-        //        "react/no-unescaped-entities": "error",
-        //        "react/require-default-props": "error",
-        //        "react/forbid-prop-types": "error"
+
+        // "arrow-parens": ["error", "always"],
+        // "react/jsx-uses-react": "error",
+        // "react/jsx-uses-vars": "error",
+        // "react/jsx-filename-extension": "error",
+        // "react/self-closing-comp": "error",
+        // "react/no-unescaped-entities": "error",
+        // "react/require-default-props": "error",
+        // "react/forbid-prop-types": "error",
+        // "indent": ["error", 2, { SwitchCase: 1 }],
+        // "linebreak-style": ["error", "unix"],
+        // "quotes": ["error", "single"],
 
     }
 
